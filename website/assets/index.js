47969c5e13d9225d5b2356998779210ac8b111ac
Jesus Perez <jperezsiculaba@serempre.com>
2022-12-12 19:25:35 -0500

    Auto Pause plugin

--- website/assets/index.js
+++ website/assets/index.js
@@ -1,11 +1,15 @@
 import MediaPlayer from "./MediaPlayer.js"
 import AutoPlay from "../plugins/AutoPlay.js"
+import AutoPause from "../plugins/AutoPause.js"
 
 const video = document.querySelector('video')
 const playButton = document.querySelector('#playButton')
 const muteButton = document.querySelector('#muteButton')
 
-const player = new MediaPlayer({el:video, plugins:[new AutoPlay()]})
+const player = new MediaPlayer({
+    el:video, 
+    plugins:[new AutoPlay(), new AutoPause()]
+})
 
 playButton.onclick = () => player.togglePlay()
 muteButton.onclick = () => player.toggleMute()
