33b91afb1682af2098337f6b8e2debf5fde2927d
Jesus Perez <jperezsiculaba@serempre.com>
2022-12-16 15:23:08 -0500

    Service workers

--- website/assets/index.js
+++ website/assets/index.js
@@ -13,3 +13,9 @@ const player = new MediaPlayer({
 
 playButton.onclick = () => player.togglePlay()
 muteButton.onclick = () => player.toggleMute()
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').catch(error => {
+        console.log(error.message)
+    })
+}
--- website/plugins/AutoPause.js
+++ website/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause{
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
 
     run(player){
@@ -10,6 +11,7 @@ class AutoPause{
             threshold: this.threshold
         })
         observer.observe(player.media)
+        document.addEventListener('visibilitychange', this.handleVisibilityChange)
     }
 
     handleIntersection(entries){
@@ -18,7 +20,16 @@ class AutoPause{
 
         if (isVisible){
             this.player.play()
-        }else {
+        } else {
+            this.player.pause()
+        }
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === 'visible'
+        if (isVisible) {
+            this.player.play()
+        } else {
             this.player.pause()
         }
     }
