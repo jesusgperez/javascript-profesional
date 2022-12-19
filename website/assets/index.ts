5f5279beecb414cf39f2393d9566227537e523d3
Jesus Perez <jperezsiculaba@serempre.com>
2022-12-19 18:29:24 -0500

    Adding ads to media player, but implementing Singleton

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from "./MediaPlayer"
 import AutoPlay from "../plugins/AutoPlay"
 import AutoPause from "../plugins/AutoPause"
+import Ads from "../plugins/ads"
 
 const video = document.querySelector('video')
 const playButton:HTMLElement = document.querySelector('#playButton')!
@@ -8,7 +9,7 @@ const muteButton:HTMLElement = document.querySelector('#muteButton')!
 
 const player = new MediaPlayer({
     el:video, 
-    plugins:[new AutoPlay(), new AutoPause()]
+    plugins:[new AutoPlay(), new AutoPause(), new Ads()]
 })
 
 playButton.onclick = () => player.togglePlay()
